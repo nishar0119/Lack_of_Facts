eed12af7e3178568ad475de39ffa7b586c0f2a11
nishar0119 <64295502+nishar0119@users.noreply.github.com>
2020-10-18 04:16:38 -0400

    Add files via upload

--- GTHACKS/app.py
+++ GTHACKS/app.py
@@ -8,7 +8,7 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import accuracy_score
 
 app = Flask(__name__)
-model = pickle.load(open('models.pkl', 'rb'))
+#model = pickle.load(open('models.pkl', 'rb'))
 
 
 
